da0439517b9adbcac9bc6d9e677c2741cd87bcb0
dpi <pro@danielph.in>
2025-06-02 17:14:53 +0800

    ✅ Testing Framework: regression testing, unit testing  (#13)

---
+++ Lists/NswLists.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PreviousNext\Ds\Nsw\Lists;
+
+final class NswLists {
+
+  /**
+   * @var class-string<\Pinto\List\ObjectListInterface>
+   */
+  public const Lists = [
+    NswAtoms::class,
+    NswComponents::class,
+    NswGlobal::class,
+    NswLayouts::class,
+  ];
+
+}
