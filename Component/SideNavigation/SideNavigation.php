9afc7f2f18b9e7d9d7647fe5a69c2554b512a858
Mohit Aghera <mohit.beta@gmail.com>
2026-01-22 21:30:53 +0530

    Test commit

---
+++ Component/SideNavigation/SideNavigation.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PreviousNext\Ds\Nsw\Component\SideNavigation;
+
+use Pinto\Attribute\Asset\Css;
+use Pinto\Slots;
+use PreviousNext\Ds\Common\Component as CommonComponent;
+use PreviousNext\Ds\Nsw\Utility;
+use PreviousNext\IdsTools\Scenario\Scenarios;
+
+#[Css('side-navigation.css', preprocess: TRUE)]
+#[Slots\Attribute\RenameSlot(original: 'parentLink', new: 'parent')]
+#[Slots\Attribute\RenameSlot(original: 'menuTrees', new: 'items')]
+#[Scenarios([CommonComponent\SideNavigation\SideNavigationScenarios::class])]
+class SideNavigation extends CommonComponent\SideNavigation\SideNavigation implements Utility\NswObjectInterface {
+  use Utility\ObjectTrait;
+
+  protected function build(Slots\Build $build): Slots\Build {
+    return parent::build($build)
+      ->set('id', $this->id)
+      ->set('title', $this->title)
+      ->set('menuTrees', $this->toArray())
+      ->set('parentLink', $this->parentLink);
+  }
+
+}
