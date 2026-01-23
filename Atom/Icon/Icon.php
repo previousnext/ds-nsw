a8a640df61fe5c064a4d5a99fe52f45257eeba68
Mohit Aghera <mohit.beta@gmail.com>
2026-01-23 09:45:52 +0530

    ⬆️ @pnx-mixtape/nswds v0.0.13 (#185)

--- Atom/Icon/Icon.php
+++ Atom/Icon/Icon.php
@@ -5,13 +5,14 @@ declare(strict_types=1);
 namespace PreviousNext\Ds\Nsw\Atom\Icon;
 
 use Pinto\Attribute\Asset\Css;
+use Pinto\Attribute\Asset\ExternalCss;
 use Pinto\Slots;
 use PreviousNext\Ds\Common\Atom as CommonAtom;
 use PreviousNext\Ds\Nsw\Utility;
 use PreviousNext\IdsTools\Scenario\Scenarios;
 
 /**
- * Icon name ($icon) is https://fonts.google.com/icons.
+ * Icon name ($icon) is https://fonts.google.com/icons?icon.set=Material+Icons.
  */
 #[Css('icon.css', preprocess: TRUE)]
 #[Slots\Attribute\RenameSlot(original: 'containerAttributes', new: 'attributes')]
@@ -19,6 +20,7 @@ use PreviousNext\IdsTools\Scenario\Scenarios;
   'size',
 ])]
 #[Scenarios([IconScenarios::class])]
+#[ExternalCss('//fonts.googleapis.com/icon?family=Material+Icons')]
 class Icon extends CommonAtom\Icon\Icon implements Utility\NswObjectInterface {
 
   use Utility\ObjectTrait;
