8818725cb12b167f6cc7d71666cf266864f91a41
dpi <pro@danielph.in>
2026-02-11 11:26:33 +0800

    chore(linkedimage): NSW `LinkedImage` fixups (#213)

--- Atom/LinkedImage/LinkedImage.php
+++ Atom/LinkedImage/LinkedImage.php
@@ -7,15 +7,21 @@ namespace PreviousNext\Ds\Nsw\Atom\LinkedImage;
 use Pinto\Slots;
 use PreviousNext\Ds\Common\Atom as CommonAtoms;
 use PreviousNext\Ds\Nsw\Utility;
+use PreviousNext\IdsTools\Scenario\Scenarios;
 
 #[Slots\Attribute\RenameSlot(original: 'link', new: 'href')]
 #[Slots\Attribute\RenameSlot(original: 'containerAttributes', new: 'attributes')]
+#[Slots\Attribute\ModifySlots(add: [
+  'title',
+])]
+#[Scenarios([CommonAtoms\LinkedImage\LinkedImageScenarios::class])]
 class LinkedImage extends CommonAtoms\LinkedImage\LinkedImage implements Utility\NswObjectInterface {
 
   use Utility\ObjectTrait;
 
   protected function build(Slots\Build $build): Slots\Build {
     return $build
+      ->set('title', $this->link->title)
       ->set('link', $this->link->href)
       ->set('image', $this->image);
   }
